510c4a7dba333e06849a2f99ca47fb90f002b415
Shamil Keheliya Muhandiram <62515133+shamilkeheliya@users.noreply.github.com>
2022-10-04 12:16:21 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-900">
       <Head>
-        <title>Lets celebrate</title>
-        <meta name="description" />
+        <title>Hacktoberfest 2022</title>
+        <meta name="description" content="Hacktoberfest: a month-long celebration of open-source projects, their maintainers, and the entire community of contributors.">
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎉</text></svg>"
