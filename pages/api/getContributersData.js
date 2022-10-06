2a1798739110a6082bb6382a93c19446872d7fa8
Prineth Fernando <70090385+prineth@users.noreply.github.com>
2022-10-06 12:38:25 +0530

    Update getContributersData.js

--- pages/api/getContributersData.js
+++ pages/api/getContributersData.js
@@ -17,14 +17,14 @@ export default function handler(req, res) {
       linkedin: "https://linkedin.com/in/uzair-dev",
     },
     {
-      id: 2,
+      id: 3,
       name: 'Ugur Atmaca',
       img: 'https://avatars.githubusercontent.com/u/1944295?v=4',
       github: 'https://github.com/uguratmaca',
       linkedin: 'https://www.linkedin.com/in/atmacaugur/',
     },
     {
-      id: 2,
+      id: 4,
       name: 'Shamil Keheliya',
       img: 'https://avatars.githubusercontent.com/u/62515133?v=4',
       github: 'https://github.com/shamilkeheliya',
